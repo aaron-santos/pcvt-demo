4db435be016ba6d9000dc9039d445548c6d9c197
Aaron Santos <aaronds109@gmail.com>
2015-05-23 15:16:56 -0700

    Add demos

--- src/los/core.clj
+++ src/los/core.clj
@@ -4,12 +4,18 @@
             [clojure.walk :as w]
             [clojure.zip :as z]
             [quil.core :as q]
-            [quil.middleware :as m]))
+            [quil.middleware :as m])
+  (:import [javax.swing JOptionPane]))
 
 (def ^:dynamic *width* 600)
 (def ^:dynamic *height* 600)
 (def ^:dynamic *cell-width* 10)
 (def ^:dynamic *cell-height* 10)
+
+(def ^:dynamic *cells-width* (/ *width* *cell-width*))
+(def ^:dynamic *cells-height* (/ *height* *cell-height*))
+
+(def radius (atom 20))
 
 (def center [(/ *width* 2) (/ *height* 2)])
 (def cell-center [(/ *width* 2 *cell-width*) (/ *height* 2 *cell-height*)])
@@ -22,10 +28,13 @@
        (println ~msg)
        result#)))
 
+(def empty-grid
+  (vec (repeat (/ *height* *cell-height*) (vec (repeat (/ *width* *cell-width*) false)))))
+  
 (defn setup []
   (q/smooth)
   (q/frame-rate 60)
-  (vec (repeat (/ *height* *cell-height*) (vec (repeat (/ *width* *cell-width*) false)))))
+  empty-grid)
 
 (defn draw-grid [size]
   (q/stroke 80)
@@ -50,7 +59,15 @@
             maxdiff (max (Math/abs xdiff) (Math/abs ydiff))
             dx (/ xdiff maxdiff)
             dy (/ ydiff maxdiff)]
-        (map (fn [i] [(Math/round (double (+ x1 (* i dx)))) (Math/round (double (+ y1 (* i dy))))])
+        (map (fn [i]
+          (let [x (double (+ x1 (* i dx)))
+                y (double (+ y1 (* i dy)))]
+          [(if (pos? x)
+              (Math/round x)
+              (- (Math/round (- x))))
+           (if (pos? y)
+              (Math/round y)
+              (- (Math/round (- y))))]))
             (range (inc maxdiff))))))))
 
 
@@ -208,8 +225,19 @@
     (cond
       (z/end? t) ks
       (empty? (z/node t)) (recur (z/next t) ks)
-      (z/branch? t) (let [new-keys (set (keys (z/node t)))]
-        (recur (z/next t) (clojure.set/union new-keys ks)))
+      (z/branch? t) (let [new-keys (keys (z/node t))]
+        (recur (z/next t) (reduce conj ks new-keys)))
+      :leaf
+      (println "got leaf" (z/node t)))))
+
+#_(defn trie->keys [trie]
+  (loop [t  (z/zipper map? vals #(zipmap (keys %1) %2) trie)
+         ks (transient #{})]
+    (cond
+      (z/end? t) (persistent! ks)
+      (empty? (z/node t)) (recur (z/next t) ks)
+      (z/branch? t) (let [new-keys (keys (z/node t))]
+        (recur (z/next t) (reduce conj! ks new-keys)))
       :leaf
       (println "got leaf" (z/node t)))))
 
@@ -231,7 +259,7 @@
   (apply hash-map (mapcat (fn [k]
                          (let [c-points (square-points 0 0 k)
                                segments     (remove empty?
-                                              (map #(line-segment [0 0] %)
+                                              (map (fn [[x y]] (line-segment [0 0] [x y]))
                                                    c-points))
                                ;; truncate segments to radius
                                segments (remove-points-beyond-radius 0 0 k segments)
@@ -260,16 +288,13 @@
   (let [diam 300
        [cx cy] center
        [ccx ccy] cell-center]
-    ;; color middle cell red
-    (q/fill 200 0 0)
-    (q/rect cx cy 8 8)
     ;; draw fine red circle over shaded cells
     (q/stroke 200 0 0)
     (q/fill 0 0 0 0)
     (q/ellipse (+ 5 cx) (+ cy 5) diam diam)
     (let [;c-points (points-in-circle ccx ccy 25)
           r (mod (int (/ (q/frame-count) 10)) 30)
-          r 20
+          r @radius
           ;c-points (circle-points ccx ccy (mod (int (/ (q/frame-count) 10)) 25))
           c-points (get r->c-points r)
           ;_ (println "points-in-circle" c-points)
@@ -288,24 +313,30 @@
           segments (trie->paths trie)
           num-segments (count segments)
           ;_ (println "culled-trie" trie)
-          visible-points (remove nil? (trie->keys trie))]
+          visible-points (set (remove nil? (trie->keys trie)))]
       ;(println "visible points" visible-points)
       ;; print visible cells
-      (q/fill 90 90 90 255)
+      (q/fill 101 100 20 255)
       (doseq [[x y] visible-points]
           ;(println "visible-point" x y)
           (q/rect (* (+ x ccx) *cell-width*) (* (+ y ccy) *cell-height*) *cell-width* *cell-height*))
       ;; shade cells to form circle
-      (q/fill 90 180 90 255)
-      (doseq [[x y] c-points]
+      #_(q/fill 90 180 90 255)
+      #_(doseq [[x y] c-points]
         ;(println "perimeter cell" x y)
         (q/rect (+ (* (+ x ccx) *cell-width*) 1)  (+ (* (+ y ccy) *cell-height*) 1)  9 9))
 
+      ;; color middle cell red
+      (q/fill 200 0 0)
+      (q/rect cx cy 8 8)
+
       ;; draw blocking cells
-      (q/fill 190 190 190 255)
       (doseq [[y line] (map-indexed vector state)]
         (doseq [[x cell] (map-indexed vector line)]
           (when cell
+            (if (contains? visible-points [(- x ccx) (- y ccy)])
+              (q/fill 158 158 86 255)
+              (q/fill 66 66 66 255))
             ;(println "Drawing rect" (* x *cell-width*) (* y *cell-height*) *cell-width* *cell-height* "for cell" cell)
             (q/rect (* x *cell-width*) (* y *cell-height*) *cell-width* *cell-height*))))
     
@@ -313,9 +344,9 @@
       ;; print line segments
       ;(println "segments")
       ;(clojure.pprint/pprint (segments->tree segments))
-      (q/stroke-weight 2)
-      (q/color-mode :hsb num-segments 1.0 1.0)
-      (doseq [[idx segment] (map-indexed vector segments)]
+      #_(q/stroke-weight 2)
+      #_(q/color-mode :hsb num-segments 1.0 1.0)
+      #_(doseq [[idx segment] (map-indexed vector segments)]
         (doseq [[[x0 y0] [x1 y1]]  (partition 2 (-> (interleave segment segment) rest butlast))]
           (q/stroke idx 1.0 0.7)
           (q/line (+ (* 10 x0) 5 cx)
@@ -332,13 +363,136 @@
     (print "collision-point-set" (collision-point-set new-state))
     new-state))
     
+(def pillar-near
+  (let [[x y] cell-center]
+    (assoc-in empty-grid [y (inc x)] true)))
+
+(def pillar-away
+  (let [[x y] cell-center]
+    (assoc-in empty-grid [(inc y) (+ x 3)] true)))
+
+(def corner-peeking
+  (let [[cx cy] cell-center]
+  (as-> empty-grid state
+    (reduce (fn [state x] (assoc-in state [(dec cy) x] true))
+            state
+            (range *cells-width*))
+    (reduce (fn [state x] (assoc-in state [(inc cy) x] true))
+            state
+            (range cx))
+    (reduce (fn [state x] (assoc-in state [(inc cy) x] true))
+            state
+            (range (inc cx) *cells-width*)))))
+
+(def diagonal-walls
+  (reduce (fn [state x]
+            (assoc-in state [(inc x) x] true))
+          empty-grid
+          (range *cells-width*)))
+
+(defn random []
+  (let [random-grid (reduce (fn [state [x y]]
+                              (if (zero? (rand-int 10))
+                                (assoc-in state [y x] true)
+                                state))
+                            empty-grid
+                            (for [x (range *cells-width*)
+                                  y (range *cells-height*)]
+                              [x y]))
+        random-grid (assoc-in random-grid (reverse cell-center) false)]
+    random-grid))
+
+(defn visible-points [state trie [x y]]
+  "Set of points visible from [x y]"
+  (if (get-in state [y x])
+    nil
+    (let [cx x
+          cy y
+          collision-points (set (map (fn [[x y]]
+                                       [(- x cx) (- y cy)])
+                                     (collision-point-set state)))
+          ;_ (println "collision-points" collision-points)
+          trie         (cull-trie collision-points trie)
+          visible-points (set (remove nil? (trie->keys trie)))]
+      ;(println "xy" [x y])
+      ;(println "cx cy" [cx cy])
+      ;(println "visible points" visible-points)
+      ;(contains? visible-points [x y])
+      visible-points)))
+
+(defn print-symmetry [state]
+  (let [r       @radius
+        trie    (get r->trie r)
+        [ccx ccy] cell-center
+        ; visible points in trie-space
+        visible-pts (visible-points state trie cell-center)
+        ; hidden-points (clojure.set/difference ks visible-points)
+        groups     (->> visible-pts
+                     ; remove blocking cells from test
+                     ; comvert trie-space to grid-space
+                     (remove (fn [[x y]] (get-in state [(+ y ccy) (+ x ccx)])))
+                     ; Is cell-center visible from trie-space point [x y]?
+                     ; group the number of symmetric and asymmetric results
+                     (group-by (fn [[x y]]
+                                 (let [transitive-visible-points (visible-points state trie [(+ x ccx) (+ y ccy)])]
+                                   (contains? transitive-visible-points [(- x) (- y)])))))
+        {sym  true
+         asym false
+         :or {sym  []
+              asym []}} groups
+        n-sym   (count (get groups true))
+        n-asym  (count (get groups false))
+        pct-sym (* 100.0 (/ n-asym (+ n-sym n-asym)))]
+    (println "testing visible points" visible-points)
+    (println "cell-center" cell-center)
+    (println "sym" (get groups true))
+    (println "asym" (get groups false))
+    (println "num symmetric-cells" n-sym)
+    (println "num asymmetric-cells" n-asym)
+    (println "Symmetry error" pct-sym "%")
+    (JOptionPane/showMessageDialog
+      nil
+      (str "Symmetry error " pct-sym "%"))
+  pct-sym))
+
+(defn print-multi-random-symmetry [state]
+  (let [iterations 100
+        errors (map (fn [i]
+                      (println "Calculating symmetry for iteration" i)
+                      (print-symmetry (random)))
+                    (range iterations))
+        error-avg (/ (reduce + errors) iterations)
+        msg (format "Iterations %d\nAverageError %f"
+              iterations
+              error-avg)]
+    (JOptionPane/showMessageDialog nil msg)))
+
+(defn on-key [state event]
+  (case (get event :key)
+    :n pillar-near
+    :a pillar-away
+    :c corner-peeking
+    :d diagonal-walls
+    :r (random)
+    :s (do (print-symmetry state)
+           state)
+    :S (do (print-multi-random-symmetry state)
+           state)
+    :+ (do (swap! radius inc)
+           (println "New Radius" @radius)
+           state)
+    :- (do (swap! radius dec)
+           (println "New Radius" @radius)
+           state)
+    state))
 
 (q/defsketch example
-  :title "Oh so many grey circles"
+  :title "Pre-computed visibility tries demo"
   :middleware [m/fun-mode]
   :setup setup
   :draw draw
   :mouse-clicked on-click
+  :key-typed on-key
   :size [*width* *height*])
 
 (defn -main [] nil)
